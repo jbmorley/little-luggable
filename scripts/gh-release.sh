2e737b7bebb208ec3271720e926d2c0f7050de57
Jason Morley <hello@jbmorley.co.uk>
2026-07-18 19:13:38 -0700

    fix: 📦 Don’t flag GitHub releases as pre-release (#31)

--- scripts/gh-release.sh
+++ scripts/gh-release.sh
@@ -25,11 +25,7 @@ set -o pipefail
 set -x
 
 # Actually make the release.
-FLAGS=()
-if $CHANGES_INITIAL_DEVELOPMENT ; then
-    FLAGS+=("--prerelease")
-fi
-gh release create "$CHANGES_TAG" --title "$CHANGES_TITLE" --notes-file "$CHANGES_NOTES_FILE" "${FLAGS[@]}"
+gh release create "$CHANGES_TAG" --title "$CHANGES_TITLE" --notes-file "$CHANGES_NOTES_FILE"
 
 # Upload the attachments.
 for attachment in "$@"
