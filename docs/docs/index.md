0b60dcbc0c30fc8b788653e07dcea8b385de5ef1
Jason Morley <hello@jbmorley.co.uk>
2026-08-10 14:58:51 -1000

    docs(website): 🌐 Add a first-cut website (#48)

---
+++ docs/docs/index.md
@@ -0,0 +1,5 @@
+---
+title: Documentation
+layout: documentation
+shows_title: false
+---
---
+++ scripts/build-website.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2023-2026 Jason Morley
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+set -e
+set -o pipefail
+set -x
+set -u
+
+SCRIPTS_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+ROOT_DIRECTORY="$SCRIPTS_DIRECTORY/.."
+WEBSITE_DIRECTORY="$ROOT_DIRECTORY/docs"
+
+# Process the command line arguments.
+POSITIONAL=()
+SERVE=false
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in
+        -s|--serve)
+        SERVE=true
+        shift
+        ;;
+        *)
+        POSITIONAL+=("$1")
+        shift
+        ;;
+    esac
+done
+
+# Update the release notes.
+"$SCRIPTS_DIRECTORY/update-release-notes.sh"
+
+# Install the Jekyll dependencies.
+cd "$WEBSITE_DIRECTORY"
+bundle install
+
+# Determine the version.
+VERSION_NUMBER=`changes version`
+export VERSION_NUMBER
+
+# Build the website.
+cd "$WEBSITE_DIRECTORY"
+if $SERVE ; then
+    bundle exec jekyll serve --watch
+else
+    bundle exec jekyll build
+fi
