101ccec1803ddcdf998927d0100c5b223e70bec7
Jason Morley <hello@jbmorley.co.uk>
2026-08-24 11:07:10 +0100

    Clean the script up a little more

--- scripts/make-sequential.py
+++ scripts/make-sequential.py
@@ -9,12 +9,14 @@ import re
 ROOT_DIRECTORY = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 GALLERY_DIRECTORY = os.path.join(ROOT_DIRECTORY, "images", "gallery")
 
-print(GALLERY_DIRECTORY)
+
+def main():
+    os.chdir(GALLERY_DIRECTORY)
+    for i, f in enumerate(sorted(glob.glob("*.jpg"))):
+        print(f"Renaming '{f}'...")
+        name = "little-luggable-%03d.jpg" % (i, )
+        os.rename(f, name)
 
 
-os.chdir(GALLERY_DIRECTORY)
-
-for i, f in enumerate(sorted(glob.glob("*.jpg"))):
-    print(f"Renaming '{f}'...")
-    name = "little-luggable-%03d.jpg" % (i, )
-    os.rename(f, name)
+if __name__:
+    main()
